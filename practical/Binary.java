0f142d27ba3c679856bb9de83e52d91612df29a8
Sukhdeep Singh <111453478+SukhiBhatti@users.noreply.github.com>
2024-11-11 21:02:29 +0530

    Complete Practical file with pdf

---
+++ practical/Binary.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class Binary {
+
+  Scanner obj = new Scanner(System.in);
+
+  int a = obj.nextInt();
+
+  public int display() {
+
+    int r = a % 2;
+    a = a / 2;
+    if (a != 0) {
+      display();
+
+    }
+    
+      System.out.print(r);
+      return a;
+  }
+
+  public static void main(String[] args) {
+
+    Binary sc = new Binary();
+
+    sc.display();
+
+  }
+}
---
+++ practical/Calculator.java
@@ -0,0 +1,65 @@
+import java.util.Scanner;
+
+public class Calculator {
+
+    static int i;
+
+   
+    public static void main(String args[]){
+ try{
+         Scanner sc= new Scanner(System.in);
+          
+      double num1, num2;
+         System.out.println("Enter first number--");
+         num1=sc.nextDouble();
+         System.out.println("Enter Second number--");
+         num2=sc.nextDouble();
+        
+         System.out.println("Select operator--");
+         char op= sc.next().charAt(0);
+         double r=0;
+         
+
+         switch (op) {
+             case '+':
+                 r = num1 + num2;
+                 break;
+
+             case '-':
+                 r = num1 - num2;
+                 break;
+
+             case '*':
+                 r = num1 * num2;
+                 break;
+
+             case '/':
+                 r = num1 / num2;
+                 break;
+
+             default:
+                 System.out.println("***Enter a valid operator***");
+                 i = 5;
+
+         }
+         sc.close();
+         if (i != 5) {
+             System.out.println("The result is:");
+             System.out.println(num1 + " " + op + " " + num2 + " = " + r);
+         }
+
+    }
+
+            catch(Exception e){
+                System.out.println(e.getMessage());
+            }
+            
+}
+}
+
+
+
+ 
+
+    
+
